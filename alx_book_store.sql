b41540dd98d14b0e12e762deb3605874d14e2b6c
abdellah el ouachama <abdellahelouchaa@gmail.com>
2024-10-20 11:35:19 +0100

    create a script to define database schema and tables

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE alx_book_store;
+CREATE DATABASE IF NOT EXISTS alx_book_store;
 CREATE TABLE Books (book_id INT PRIMARY KEY, title VARCHAR(130), author_id INT, price DOUBLE, publication_date DATE, FOREIGN KEY (author_id) REFERENCES Authors(author_id));
 CREATE TABLE Authors (author_id INT PRIMARY KEY, author_name VARCHAR(215));
 CREATE TABLE Customers (customer_id INT PRIMARY KEY, customer_name VARCHAR(215), email VARCHAR(215), adress TExT);
